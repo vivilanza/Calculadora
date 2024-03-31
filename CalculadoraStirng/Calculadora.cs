0e504e101af29a25d234c60dc7a73b8230c73fd6
vivilanza <celiaviviana@icloud.com>
2024-03-31 00:46:31 -0300

    Green - sumar un numero

--- CalculadoraStirng/Calculadora.cs
+++ CalculadoraStirng/Calculadora.cs
@@ -5,6 +5,13 @@ public class Calculadora
     private const int VALOR_POR_DEFECTO = 0; //Snake_case se usa solo para las constantes separar con una barra baja
     public int Sumar(string numeros)
     {
+        if (numeros.Length == 1)
+        {
+            return int.Parse(numeros);
+        }
+        
+            
+        
         return VALOR_POR_DEFECTO;
     }
 }
