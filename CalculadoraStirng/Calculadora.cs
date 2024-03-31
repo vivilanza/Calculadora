c221bb40334078289f32a9455f8c8d8ced64fb90
vivilanza <celiaviviana@icloud.com>
2024-03-31 00:52:49 -0300

    Refactir sumar un numero

--- CalculadoraStirng/Calculadora.cs
+++ CalculadoraStirng/Calculadora.cs
@@ -5,13 +5,21 @@ public class Calculadora
     private const int VALOR_POR_DEFECTO = 0; //Snake_case se usa solo para las constantes separar con una barra baja
     public int Sumar(string numeros)
     {
-        if (numeros.Length == 1)
+        if (EsSoloUnNumero(numeros) )
         {
-            return int.Parse(numeros);
+            return ConvertirStringAInt(numeros);
         }
         
-            
-        
         return VALOR_POR_DEFECTO;
+    }
+
+    private static int ConvertirStringAInt(string numeros)
+    {
+        return int.Parse(numeros);
+    }
+
+    private static bool EsSoloUnNumero(string numeros)
+    {
+        return numeros.Length == 1;
     }
 }
