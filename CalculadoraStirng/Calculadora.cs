b0f8d2f9d997af86d59035626463a67a7466a414
vivilanza <celiaviviana@icloud.com>
2024-03-31 00:40:50 -0300

    Refactor - hice el valor por defecto en calculadora

--- CalculadoraStirng/Calculadora.cs
+++ CalculadoraStirng/Calculadora.cs
@@ -2,8 +2,9 @@
 
 public class Calculadora
 {
-    public int Sumar(string v)
+    private const int VALOR_POR_DEFECTO = 0; //Snake_case se usa solo para las constantes separar con una barra baja
+    public int Sumar(string numeros)
     {
-        return 0;
+        return VALOR_POR_DEFECTO;
     }
 }
