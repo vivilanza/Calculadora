22c1ce9a2b627271c5a0098351a349cc0dfd93a1
vivilanza <celiaviviana@icloud.com>
2024-03-31 00:09:15 -0300

    Primer commit

---
+++ CalculadorStringTest/CalculadoraTest.cs
@@ -0,0 +1,16 @@
+namespace CalculadorStringTest;
+
+[TestClass]
+public class CalculadoraTest
+{
+    [TestMethod]
+    public void SumarStirngVacioTest()
+    {
+        Calculadora unaCalculadora = new Calculadora();
+        Assert.AreEqual(0,unaCalculadora.Sumar(""));
+    }
+    [TestMethod]
+    public void
+    
+    
+}
