6ef345af34df2def956734a2651780178cfeca4c
vivilanza <celiaviviana@icloud.com>
2024-03-31 01:36:15 -0300

    Green - sumar dos numeros con barra

--- CalculadoraStirng/Calculadora.cs
+++ CalculadoraStirng/Calculadora.cs
@@ -3,25 +3,31 @@
 public class Calculadora
 {
     private const int VALOR_POR_DEFECTO = 0; //Snake_case se usa solo para las constantes separar con una barra baja
-    private const char Separador = ',';
+    private const string Separador = ",";
     public int Sumar(string numeros)
     {
+        numeros = numeros.Replace('\n', ConvertirSeparador());
         if (EsSoloUnNumero(numeros) )
         {
             return ConvertirStringAInt(numeros);
         }
 
-        if (ContieneSeparador(numeros))
+        if (SonVariosNumeros(numeros))
         {
-            string[] valores = numeros.Split(Separador); 
+            string[] valores = numeros.Split(ConvertirSeparador()); 
             return ConvertirStringAInt(valores[0]) + ConvertirStringAInt(valores[1]);
         }
 
         return VALOR_POR_DEFECTO;
     }
 
+    private static char ConvertirSeparador()
+    {
+        return Separador.ToCharArray().First();
+    }
+
    
-    private static bool ContieneSeparador(string numeros)
+    private static bool SonVariosNumeros(string numeros)
     {
         return numeros.Contains(Separador);
     }
